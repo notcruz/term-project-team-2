4697c327faa64acaa0624fc0bfd2c5dfb93a60b3
bgc7753 <bgc7753@g.rit.edu>
2022-10-31 17:14:47 -0400

    Remove aws creds, export LabRole to variable

--- main.tf
+++ main.tf
@@ -26,12 +26,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-
-  access_key = "ASIAQCQUY6GCZ6TFQMOP"
-
-  secret_key = "E7NOu2ZsIXT8KQ4TXfXKgEbyMzIbD844ZJf5lvdR"
-
-  token = "FwoGZXIvYXdzEJD//////////wEaDI3m/tk4FXqrlMhNRiK/AZHFZVOxOp35DvGvl3/sI4Hexnp7a/I3NE4l7I5JtPnMK+B2fWZA4RFOe5Hz729/g6ZkqqY5D7hix2xdNf30PJkExRjciXCpCdoH1+WZZZn2EMrSo1/aixxlGf2bzKS0pLaGKzuCluHnrx0TPUrG6/jbZuX2sEskvndYp6UMyKdEhkiTICswa3LDFHIbDzt+ccYm8ibYXS+HTpt6VjzRPP3dRbwPhXtnrdI7U/+u1/DhQwD45/9NKywro7oHjMkcKJqA/JoGMi0OiJbfT7DP1VdhtxSoNICrCQcua8KBWjBjzdkW/u0VRYZY+ZyuKhzbpV/ito0="
 }
 
 /*
@@ -42,7 +36,7 @@ provider "aws" {
 
 resource "aws_sfn_state_machine" "sfn_state_machine" {
   name     = "main-step-function"
-  role_arn = "arn:aws:iam::005412286853:role/LabRole"
+  role_arn = var.lab_role_arn
 
   definition = <<EOF
 {
