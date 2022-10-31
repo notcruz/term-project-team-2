0cf0b7454efc127a1cc0a6d0434304e02c6ef4f0
bgc7753 <bgc7753@g.rit.edu>
2022-10-31 18:56:02 -0400

    Write analysis to DynamoDB

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,8 @@
 import json
 import boto3
 
+from collections import defaultdict
+
 def analysis_handler(event, context):
     # Filename passed in as query string parameter (ex. fileName=tweets.txt)
 
@@ -9,6 +11,8 @@ def analysis_handler(event, context):
     bucket = "rit-cloud-team-2-raw-data-bucket-test"
 
     file_name = event['queryStringParameters']['fileName']
+    name = event['queryStringParameters']['name']
+    postDeath = "True" if event['queryStringParameters']['postDeath'] == "True" else "False"
     text = s3.get_object(Bucket = bucket, Key = file_name)
     tweets = text['Body'].read().decode('utf-8').split('\n')
     # Split text file up into an array so each individual tweet is analyzed
@@ -17,10 +21,11 @@ def analysis_handler(event, context):
     # [ANALYSIS CODE GOES HERE]
     # Lists to hold sentiment labels and sentiment scores
     sentiments = []
-    pos_score = []
-    neg_score = []
-    neutral_score = []
-    mixed_score = []
+    sentiment_counts = defaultdict(int)
+    pos_scores, pos_aggr_score = [], 0
+    neg_scores, neg_aggr_score = [], 0
+    neutral_scores, neutral_aggr_score = [], 0
+    mixed_scores, mixed_aggr_score = [], 0
 
     # Perform sentiment analysis on each tweet
     comprehend = boto3.client("comprehend")
@@ -30,26 +35,49 @@ def analysis_handler(event, context):
             continue
         res = comprehend.detect_sentiment(Text = tweet, LanguageCode='en')
 
-        sentiments.append(res['Sentiment'])
+        sentiment = res['Sentiment']
+        sentiments.append(sentiment)
+        sentiment_counts[sentiment] += 1
         for key, val in res['SentimentScore'].items():
             if key == "Positive":
-                pos_score.append(val)
+                pos_scores.append(val)
+                pos_aggr_score += float(val)
             if key == "Negative":
-                neg_score.append(val)
+                neg_scores.append(val)
+                neg_aggr_score += float(val)
             if key == "Neutral":
-                neutral_score.append(val)
+                neutral_scores.append(val)
+                neutral_aggr_score += float(val)
             if key == "Mixed":
-                mixed_score.append(val)
+                mixed_scores.append(val)
+                mixed_aggr_score += float(val)
+
+    num_of_tweets = len(tweets)
+    pos_aggr_score /= num_of_tweets
+    neg_aggr_score /= num_of_tweets
+    mixed_aggr_score /= num_of_tweets
+    neutral_aggr_score /= num_of_tweets
+
+    body = {
+            'Name': name,
+            'PostDeath': postDeath,
+            'PositiveScore': str(pos_aggr_score),
+            'PositiveFrequency': sentiment_counts['POSITIVE'],
+            'NegativeScore': str(neg_aggr_score),
+            'NegativeFrequency': sentiment_counts['NEGATIVE'],
+            'NeutralScore': str(neutral_aggr_score),
+            'NeutralFrequency': sentiment_counts['NEUTRAL'],
+            'MixedScore': str(mixed_aggr_score),
+            'MixedFrequency': sentiment_counts['MIXED']
+        }
 
     # STORE RESULTS IN DB, THEN RETURN RESULTS
-    dynamo = boto3.client("dynamodb")
-    body = {
-            "Sentiments": sentiments,
-            "Positive": pos_score,
-            "Negative": neg_score,
-            "Neutral": neutral_score,
-            "Mixed": mixed_score
-        }
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table('CachedPeople')
+    # Aggregated scores are string because DynamoDB does not support float.
+    response = table.put_item(
+        Item=body
+    )
 
     return {
         "statusCode": 200,
