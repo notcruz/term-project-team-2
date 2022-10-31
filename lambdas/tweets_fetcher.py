777a3a7df9d856ec165cca4bbe65be06c91604b5
Raavi Chowdhury <rkc3356@g.rit.edu>
2022-10-31 10:09:29 -0400

    Added function to collect location from user profile if unavailable on tweet object.

--- lambdas/tweets_fetcher.py
+++ lambdas/tweets_fetcher.py
@@ -1,6 +1,10 @@
 import requests
 import json
+import tweepy
+from dotenv import dotenv_values
 
+USER_CONFIG = dotenv_values("/Users/rvi/Documents/rcPersonal/Year5/Cloud/term-project-team-2/lambdas/.env")
+TW_CLIENT = tweepy.Client(USER_CONFIG['BEARER_TOKEN'])
 MAX_ITERATION = 10
 MAX_TWEET_COUNT = 20
 API_BASE_URL = "https://api.twitter.com/1.1"
@@ -27,16 +31,26 @@ def set_twitter_token(session):
     return response.ok
 
 
+def retrieve_location(tweet_geo, user_id):
+    # maximize chance of collecting geo data by checking user object if unavailable on tweet
+    if tweet_geo is None or tweet_geo == "":
+        user = TW_CLIENT.get_user(id=user_id)
+        return user[0]['location']
+    else:
+        return tweet_geo
+
+
 def parse_tweets(json):
     tweets = json["globalObjects"]["tweets"]
     return [
-        {"created_at": tweets[key]["created_at"], "id": tweets[key]["id"], "text": tweets[key]["text"], "lang": tweets[key]["lang"], "geo": tweets[key]["geo"]} for key in tweets.keys()
+        {"created_at": tweets[key]["created_at"], "id": tweets[key]["id"], "text": tweets[key]["text"], "lang": tweets[key]["lang"], "geo": retrieve_location(tweets[key]["geo"],tweets[key]["user_id"]), "user": tweets[key]["user_id"]} for key in tweets.keys()
     ]
     
 def get_next_token(data, cursor):
     if not cursor:
         return data["timeline"]["instructions"][0]["addEntries"]["entries"][-1]["content"]["operation"]["cursor"]["value"]
     return data["timeline"]["instructions"][-1]["replaceEntry"]["entry"]["content"]["operation"]["cursor"]["value"]
+
 
 def get_tweets(session, count, q):
     # shallow copy for the default params
@@ -84,10 +98,10 @@ def lambda_handler(event, context):
 
 def main():
     # include count if you want
-    input_data = {"query": "queen elizabeth", "count": 100}
-    with open("../test/lambdas/result.json", "w") as file:
+    input_data = {"query": "queen elizabeth", "count": 20}
+    with open("/Users/rvi/Documents/rcPersonal/Year5/Cloud/term-project-team-2/test/lambdas/results.json", "w") as file:
         file.write(json.dumps(json.loads(
             lambda_handler(input_data, None)["body"]), indent=4))
-
+    
 
 main()
