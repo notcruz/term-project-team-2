8d1c6a8301648d3f16a12bd969f2371d94227a4e
aep7128 <aep7128@g.rit.edu>
2022-10-30 21:10:29 -0400

    Added DynamoDB to Terraform

--- s3.tf
+++ s3.tf
@@ -1,14 +1,3 @@
-
-/*
- * Initial Cache file that's generated on deployment
- */
-
-resource "aws_s3_object" "object" {
-  bucket = "rit-cloud-team-2-cached-data-bucket"
-  key = "default_cache"
-  source = "src/default_cache.json"
-}
-
 /*
  *  ========================================
  *                S3 Buckets
@@ -17,7 +6,7 @@ resource "aws_s3_object" "object" {
  */
 
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "rit-cloud-team-2-lambda-bucket"
+  bucket = "rit-cloud-team-2-lambda-bucket-test"
 
   tags = {
     Name = "Main_Lambda_Bucket"
@@ -26,7 +15,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 }
 
 resource "aws_s3_bucket" "raw_data_bucket" {
-  bucket = "rit-cloud-team-2-raw-data-bucket"
+  bucket = "rit-cloud-team-2-raw-data-bucket-test"
 
   tags = {
     Name = "Raw_Data_Bucket"
@@ -34,12 +23,24 @@ resource "aws_s3_bucket" "raw_data_bucket" {
   }
 }
 
+/*
+ * (We probaably won't need this)
+ */
 resource "aws_s3_bucket" "cache_bucket" {
-  bucket = "rit-cloud-team-2-cached-data-bucket"
-
+  bucket = "rit-cloud-team-2-cached-data-bucket-test"
   tags = {
     Name = "Cached_Data_Bucket"
     Environment = "Dev"
   }
 }
 
+/*
+ * Initial Cache file that's generated on deployment
+ * (We probaably won't need this)
+ */
+
+resource "aws_s3_object" "object" {
+  bucket = "rit-cloud-team-2-cached-data-bucket-test"
+  key = "default_cache"
+  source = "src/default_cache.json"
+}
