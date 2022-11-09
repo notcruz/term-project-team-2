c40a517b41bcf7774941a9db6c82d5aed6bf8858
jc1831 <jc1831@rit.edu>
2022-11-09 10:06:23 -0500

    frontend

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 function App() {
   const [input, setInput] = useState();
+  const [count, setCount] = useState(50);
+  const [date, setDate] = useState();
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center">
@@ -14,28 +16,65 @@ function App() {
         </div>
         <div>
           <p className="text-lg">
-            Check the how people feel about a celebrity before and after their
+            Check how people feel about a celebrity before and after their
             passing!
           </p>
         </div>
-        <div className="mt-3">
-          <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e?.target?.value)}
-            className="bg-transparent border border-gray-300 px-3 py-1"
-          />
+        <div className="mt-6 flex flex-wrap flex-col space-y-3 items-center justify-center">
+          <div className="flex flex-col gap-y-3">
+            <div className="flex flex-col items-start">
+              <div className={"mb-0.5"}>
+                <h3 className="font-bold">Celebrity Name</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  value={input}
+                  placeholder="Queen Elizabeth II"
+                  onChange={(e) => setInput(e?.target?.value)}
+                  className="rounded-md bg-transparent border w-96 border-gray-300 px-4 py-1.5 duration-300 outline-indigo-400 ring-0"
+                />
+              </div>
+            </div>
+            <div className="flex justify-center gap-x-9">
+              <div className="flex flex-col items-start">
+                <div className={"mb-0.5"}>
+                  <h3 className="font-bold">Tweet Count</h3>
+                </div>
+                <div>
+                  <input
+                    type={"number"}
+                    value={count}
+                    placeholder={50}
+                    onChange={(e) => setCount(e?.target?.value)}
+                    className="rounded-md bg-transparent border w-44 border-gray-300 px-4 py-1.5 duration-300 outline-indigo-400 ring-0"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col items-start">
+                <div className={"mb-0.5"}>
+                  <h3 className="font-bold">Date of Death</h3>
+                </div>
+                <div>
+                  <input
+                    type={"date"}
+                    value={date}
+                    placeholder="09/08/2022"
+                    onChange={(e) => setDate(e?.target?.value)}
+                    className="rounded-md bg-transparent border border-gray-300 px-4 py-1.5 duration-300 outline-indigo-400 ring-0"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex mt-3">
+              <button className="w-full rounded-md bg-indigo-400 font-bold px-4 py-1.5 hover:scale-105 duration-300">
+                Search
+              </button>
+            </div>
+          </div>
         </div>
       </body>
     </div>
   );
 }
-
-/**
-  <nav className="bg-gray-600 flex justify-center py-3">
-        <a href="/" className="font-bold text-xl hover:scale-105 duration-300">
-          Sentimental Analysis v2
-        </a>
-      </nav>
- */
 export default App;
