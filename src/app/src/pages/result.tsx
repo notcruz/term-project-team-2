5bbe2a38e596ed9360c77bb461eda8d5065ac50e
jc1831 <jc1831@rit.edu>
2022-11-17 14:56:39 -0500

    hopeful update

--- src/app/src/pages/result.tsx
+++ src/app/src/pages/result.tsx
@@ -5,19 +5,15 @@ import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import {twMerge} from "tailwind-merge";
 
-const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
-
 const Result = ({query}: { query: QueryParams }) => {
     const [result, setResult] = useState<LambdaResponse>();
     const [type, setType] = useState<"frequency" | "score">("frequency");
     const router = useRouter();
 
-    console.log(process.env)
-
     /* fetch from api gateway and get result */
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch(`${ENDPOINT}/?name=${query.name}&count=${query.count}&death=${query.date}`)
+            const response = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/?name=${query.name}&count=${query.count}&death=${query.date}`)
                 .then((r) => r.json());
             if (response.data)
                 setResult(response.data);
